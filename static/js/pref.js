0cc4f70035a6913c20a2283a40eb6c144b3c6446
Alex <132716380+alexsky177@users.noreply.github.com>
2023-05-06 17:25:42 +0300

    Update pref.js

--- static/js/pref.js
+++ static/js/pref.js
@@ -14,3 +14,16 @@ function cleanDynamicDiv() {
   var dynamicDiv = document.getElementById("messages");
   dynamicDiv.innerHTML = "";
 }
+  document.querySelector(".mobile-sidebar").addEventListener("click", (event) => {
+  const sidebar = document.querySelector(".conversations");
+
+  if (sidebar.classList.contains("shown")) {
+    sidebar.classList.remove("shown");
+    event.target.classList.remove("rotated");
+  } else {
+    sidebar.classList.add("shown");
+    event.target.classList.add("rotated");
+  }
+
+  window.scrollTo(0, 0);
+});
