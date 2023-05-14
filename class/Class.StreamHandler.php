fb7f3172bcbea8983b4cff5d2ad4f544397c0ba8
Alex <132716380+alexsky177@users.noreply.github.com>
2023-05-15 00:50:25 +0300

    Update Class.StreamHandler.php

--- class/Class.StreamHandler.php
+++ class/Class.StreamHandler.php
@@ -2,11 +2,11 @@
 
 class StreamHandler {
 
-    private $data_buffer;//缓存，有可能一条data被切分成两部分了，无法解析json，所以需要把上一半缓存起来
-    private $counter;//数据接收计数器
-    private $qmd5;//问题md5
-    private $chars;//字符数组，开启敏感词检测时用于缓存待检测字符
-    private $punctuation;//停顿符号
+    private $data_buffer;//Caching, there is a possibility that a data is cut into two parts, can not parse the json, so you need to cache the upper half
+    private $counter;//Data reception counter
+    private $qmd5;//Issue md5
+    private $chars;//Array of characters, used to cache characters to be detected when sensitive word detection is enabled
+    private $punctuation;//Pause symbol
     private $dfa = NULL;
     private $check_sensitive = FALSE;
 
@@ -37,39 +37,39 @@ class StreamHandler {
         }
 
         /*
-            此处步骤仅针对 openai 接口而言
-            每次触发回调函数时，里边会有多条data数据，需要分割
-            如某次收到 $data 如下所示：
+            This step is only for the openai interface
+            Each time the callback function is triggered, there will be multiple pieces of data in it, which need to be split
+            If $data is received at a certain time, it looks like this：
             data: {"id":"chatcmpl-6wimHHBt4hKFHEpFnNT2ryUeuRRJC","object":"chat.completion.chunk","created":1679453169,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"role":"assistant"},"index":0,"finish_reason":null}]}\n\ndata: {"id":"chatcmpl-6wimHHBt4hKFHEpFnNT2ryUeuRRJC","object":"chat.completion.chunk","created":1679453169,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":"以下"},"index":0,"finish_reason":null}]}\n\ndata: {"id":"chatcmpl-6wimHHBt4hKFHEpFnNT2ryUeuRRJC","object":"chat.completion.chunk","created":1679453169,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":"是"},"index":0,"finish_reason":null}]}\n\ndata: {"id":"chatcmpl-6wimHHBt4hKFHEpFnNT2ryUeuRRJC","object":"chat.completion.chunk","created":1679453169,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":"使用"},"index":0,"finish_reason":null}]}
 
-            最后两条一般是这样的：
+            The last two are generally like this：
             data: {"id":"chatcmpl-6wimHHBt4hKFHEpFnNT2ryUeuRRJC","object":"chat.completion.chunk","created":1679453169,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{},"index":0,"finish_reason":"stop"}]}\n\ndata: [DONE]
 
-            根据以上 openai 的数据格式，分割步骤如下：
+            According to the above data format of openai, the segmentation steps are as follows：
         */
 
-        // 0、把上次缓冲区内数据拼接上本次的data
+        // 0、Splice the data in the last buffer with the current data
         $buffer = $this->data_buffer.$data;
         
-        //拼接完之后，要把缓冲字符串清空
+        //After stitching, clear the buffered string
         $this->data_buffer = '';
 
-        // 1、把所有的 'data: {' 替换为 '{' ，'data: [' 换成 '['
+        // 1、Replace all 'data: {' with '{' and 'data: [' with '['
         $buffer = str_replace('data: {', '{', $buffer);
         $buffer = str_replace('data: [', '[', $buffer);
 
-        // 2、把所有的 '}\n\n{' 替换维 '}[br]{' ， '}\n\n[' 替换为 '}[br]['
+        // 2、Replace all '}\n\n{' with '}[br]{', '}\n\n[' with '}[br]['
         $buffer = str_replace("}\n\n{", '}[br]{', $buffer);
         $buffer = str_replace("}\n\n[", '}[br][', $buffer);
 
-        // 3、用 '[br]' 分割成多行数组
+        // 3、Splitting into multi-line arrays with '[br]'
         $lines = explode('[br]', $buffer);
 
-        // 4、循环处理每一行，对于最后一行需要判断是否是完整的json
+        // 4、Loop through each line and determine if the last line is a complete json
         $line_c = count($lines);
         foreach($lines as $li=>$line){
             if(trim($line) == '[DONE]'){
-                //数据传输结束
+                //End of data transfer
                 $this->data_buffer = '';
                 $this->counter = 0;
                 $this->sensitive_check();
@@ -79,11 +79,11 @@ class StreamHandler {
             $line_data = json_decode(trim($line), TRUE);
             if( !is_array($line_data) || !isset($line_data['choices']) || !isset($line_data['choices'][0]) ){
                 if($li == ($line_c - 1)){
-                    //如果是最后一行
+                    //If it is the last line
                     $this->data_buffer = $line;
                     break;
                 }
-                //如果是中间行无法json解析，则写入错误日志中
+                //If it is an intermediate line that cannot be parsed by json, it is written to the error log
                 file_put_contents('./log/error.'.$this->qmd5.'.log', json_encode(['i'=>$this->counter, 'line'=>$line, 'li'=>$li], JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT).PHP_EOL.PHP_EOL, FILE_APPEND);
                 continue;
             }
@@ -97,12 +97,12 @@ class StreamHandler {
     }
 
     private function sensitive_check($content = NULL){
-        // 如果不检测敏感词，则直接返回给前端
+        // If no sensitive words are detected, they are returned directly to the front-end
         if(!$this->check_sensitive){
             $this->write($content);
             return;
         }
-    	//每个 content 都检测是否包含换行或者停顿符号，如有，则成为一个新行
+    	//Each content is checked to see if it contains a newline or a stop sign, and if so, it becomes a newline
         if(!$this->has_pause($content)){
             $this->chars[] = $content;
             return;
