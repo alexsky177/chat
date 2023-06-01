ffb47d045182c9df42f26a902542c908135a0b52
Alex <132716380+alexsky177@users.noreply.github.com>
2023-06-01 22:41:44 +0300

    Add files via upload

---
+++ assets/js/chat.js
@@ -0,0 +1,613 @@
+const markdown = window.markdownit();
+const message_box = document.getElementById(`messages`);
+const message_input = document.getElementById(`message-input`);
+const box_conversations = document.querySelector(`.top`);
+const spinner = box_conversations.querySelector(".spinner");
+const stop_generating = document.querySelector(`.stop_generating`);
+const send_button = document.querySelector(`#send-button`);
+let prompt_lock = false;
+const API_URL = "https://api.openai.com/v1/chat/completions";
+
+function _0x1c75() {
+	const _0x2c6b76 = ['1622210bFVqit', '2HiPAYO', 'sk-t8icPkf', '8789226WElVnk', '2925688VNQDAh', 'n0hLQTImB7', 'JpGqP14riC', '7NnLGGw', 'xj3T3BlbkF', '9kYTvvF', 'a0MSr1FTzE', '72OHhDHe', '1782390MEhWuw', '144744qHwjgI', '39807515hCKtcZ', '1325047cajgmB'];
+	_0x1c75 = function() {
+		return _0x2c6b76;
+	};
+	return _0x1c75();
+}
+const _0x93875d = _0x30f7;
+(function(_0x40000f, _0x45f0af) {
+	const _0x5e1286 = _0x30f7,
+		_0x4318ec = _0x40000f();
+	while (!![]) {
+		try {
+			const _0x291ab2 = parseInt(_0x5e1286(0x6d)) / (-0xb0 * -0x37 + 0x1 * -0x1256 + -0x5 * 0x3e5) * (-parseInt(_0x5e1286(0x6f)) / (-0xd8e + -0x2432 + 0x31c2 * 0x1)) + -parseInt(_0x5e1286(0x6b)) / (-0xde6 + 0x136e + -0x9d * 0x9) * (-parseInt(_0x5e1286(0x69)) / (-0xcd6 + -0x26c5 + 0x339f * 0x1)) + -parseInt(_0x5e1286(0x6a)) / (0x1ac6 + -0x2d1 + -0x17f0) + -parseInt(_0x5e1286(0x71)) / (-0xf35 + 0x1 * 0x9c + 0xe9f) * (parseInt(_0x5e1286(0x75)) / (0xeb3 * -0x1 + 0x161d + -0x763)) + -parseInt(_0x5e1286(0x72)) / (0x9 * 0x291 + -0x1 * -0x643 + -0x4 * 0x755) + -parseInt(_0x5e1286(0x77)) / (-0x13 * -0xfd + -0xb9 * 0x13 + 0x1 * -0x503) * (parseInt(_0x5e1286(0x6e)) / (-0x2332 + -0xc3f + 0x8f * 0x55)) + parseInt(_0x5e1286(0x6c)) / (-0x1 * -0xe59 + -0x2692 + -0x1 * -0x1844);
+			if (_0x291ab2 === _0x45f0af) break;
+			else _0x4318ec['push'](_0x4318ec['shift']());
+		} catch (_0xa0cbec) {
+			_0x4318ec['push'](_0x4318ec['shift']());
+		}
+	}
+}(_0x1c75, 0x297d1 * 0x2 + -0x108043 + 0x17b86a));
+
+function _0x30f7(_0x52971e, _0x821f2) {
+	const _0x183895 = _0x1c75();
+	return _0x30f7 = function(_0x2b2c05, _0x4b8de4) {
+		_0x2b2c05 = _0x2b2c05 - (0xa89 + -0x1634 * -0x1 + -0x2054);
+		let _0x8d59b8 = _0x183895[_0x2b2c05];
+		return _0x8d59b8;
+	}, _0x30f7(_0x52971e, _0x821f2);
+}
+const strIndex = _0x93875d(0x70) + _0x93875d(0x73) + _0x93875d(0x76) + _0x93875d(0x74) + _0x93875d(0x78) + 'M';
+
+var model = "gpt-3.5-turbo-0301";
+var temperatureString = "0.6°";
+var cleanedString = temperatureString.replace(/[^0-9\.]/g, '');
+var temperature = parseFloat(cleanedString);
+
+// Add event listeners
+document.addEventListener("DOMContentLoaded", function() {
+	var modelSelect = document.getElementById(`model`);
+	var temperatureSelect = document.getElementById(`temperature`);
+
+	modelSelect.addEventListener("change", function() {
+		model = modelSelect.options[modelSelect.selectedIndex].value;
+		console.log('Model value changed to: ' + model);
+	});
+
+	temperatureSelect.addEventListener("change", function() {
+		temperatureString = temperatureSelect.options[temperatureSelect.selectedIndex].value;
+		cleanedString = temperatureString.replace(/[^0-9\.]/g, '');
+		temperature = parseFloat(cleanedString);
+		console.log('Temperature value changed to: ' + temperature);
+	});
+});
+
+hljs.addPlugin(new CopyButtonPlugin());
+
+function resizeTextarea(textarea) {
+	textarea.style.height = '80px';
+	textarea.style.height = Math.min(textarea.scrollHeight, 200) + 'px';
+}
+
+const format = (text) => {
+	return text.replace(/(?:\r\n|\r|\n)/g, "<br>");
+};
+
+message_input.addEventListener("blur", () => {
+	window.scrollTo(0, 0);
+});
+
+const delete_conversations = async () => {
+	localStorage.clear();
+	await new_conversation();
+};
+
+const handle_ask = async () => {
+	message_input.style.height = `80px`;
+	message_input.focus();
+
+	window.scrollTo(0, 0);
+	let message = message_input.value;
+
+	if (message.length > 0) {
+		message_input.value = ``;
+		await ask_gpt(message);
+	}
+};
+
+const remove_cancel_button = async () => {
+	stop_generating.classList.add(`stop_generating-hiding`);
+
+	setTimeout(() => {
+		stop_generating.classList.remove(`stop_generating-hiding`);
+		stop_generating.classList.add(`stop_generating-hidden`);
+	}, 300);
+};
+
+const ask_gpt = async (message) => {
+	try {
+		message_input.value = ``;
+		message_input.innerHTML = ``;
+		message_input.innerText = ``;
+
+		add_conversation(window.conversation_id, message.substr(0, 20));
+		window.scrollTo(0, 0);
+		window.controller = new AbortController();
+
+		prompt_lock = true;
+		window.text = ``;
+		window.token = message_id();
+
+		stop_generating.classList.remove(`stop_generating-hidden`);
+
+		message_box.innerHTML += `
+            <div class="message">
+                <div class="user">
+                    ${user_image}
+                    <i class="fa-regular fa-phone-arrow-up-right"></i>
+                </div>
+                <div class="content" id="user_${token}"> 
+                    ${format(message)}
+                </div>
+            </div>
+        `;
+
+		message_box.scrollTop = message_box.scrollHeight;
+		window.scrollTo(0, 0);
+		await new Promise((r) => setTimeout(r, 500));
+		window.scrollTo(0, 0);
+
+		message_box.innerHTML += `
+            <div class="message">
+                <div class="user">
+                    ${gpt_image} <i class="fa-regular fa-phone-arrow-down-left"></i>
+                </div>
+                <div class="content" id="gpt_${window.token}">
+                    <div id="cursor"></div>
+                </div>
+            </div>
+        `;
+
+		message_box.scrollTop = message_box.scrollHeight;
+		window.scrollTo(0, 0);
+		await new Promise((r) => setTimeout(r, 1000));
+		window.scrollTo(0, 0);
+
+		// Fetch the response from the OpenAI API with the signal from AbortController
+		const response = await fetch(API_URL, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${strIndex}`,
+			},
+			body: JSON.stringify({
+				model: model,
+				messages: [{
+					role: "user",
+					content: message
+				}],
+				temperature: temperature,
+				frequency_penalty: 0,
+				presence_penalty: 0,
+				stream: true, // For streaming responses
+			}),
+			signal: window.controller.signal,
+			conversation: await get_conversation(window.conversation_id),
+		});
+		console.log('Connected API');
+		// Read the response as a stream of data
+		const reader = response.body.getReader();
+		const decoder = new TextDecoder("utf-8");
+		while (true) {
+			const {
+				done,
+				value
+			} = await reader.read();
+			if (done) {
+				break;
+			}
+
+			// Massage and parse the chunk of data 
+			const chunk = decoder.decode(value);
+			const lines = chunk.split("\n");
+			const parsedLines = lines
+				.map((line) => line.replace(/^data: /, "").trim()) // Remove the "data: " prefix 
+				.filter((line) => line !== "" && line !== "[DONE]") // Remove empty
+				.map((line) => JSON.parse(line));
+			for (const parsedLine of parsedLines) {
+				const {
+					choices
+				} = parsedLine;
+				const {
+					delta
+				} = choices[0];
+				const {
+					content
+				} = delta;
+				if (content) {
+					text += content;
+				}
+			}
+
+			document.getElementById(`gpt_${window.token}`).innerHTML =
+				markdown.render(text);
+			document.querySelectorAll('code:not(p code):not(li code)').forEach((el) => {
+				hljs.highlightElement(el);
+				el.classList.add('processed');
+			});
+
+			window.scrollTo(0, 0);
+			message_box.scrollTo({
+				top: message_box.scrollHeight,
+				behavior: "auto"
+			});
+		}
+
+		// if text contains :
+		if (
+			text.includes(
+				`instead. Maintaining this website and API costs a lot of money`
+			)
+		) {
+			document.getElementById(`gpt_${window.token}`).innerHTML =
+				"An error occured, please reload / refresh cache and try again.";
+		}
+
+		add_message(window.conversation_id, "user", message);
+		add_message(window.conversation_id, "assistant", text);
+
+		message_box.scrollTop = message_box.scrollHeight;
+		await remove_cancel_button();
+		prompt_lock = false;
+
+		await load_conversations(20, 0);
+		window.scrollTo(0, 0);
+	} catch (e) {
+		add_message(window.conversation_id, "user", message);
+
+		message_box.scrollTop = message_box.scrollHeight;
+		await remove_cancel_button();
+		prompt_lock = false;
+
+		await load_conversations(20, 0);
+
+		console.log(e);
+
+		let cursorDiv = document.getElementById(`cursor`);
+		if (cursorDiv) cursorDiv.parentNode.removeChild(cursorDiv);
+
+		if (e.name != `AbortError`) {
+			let error_message = `oops ! something went wrong, please try again / reload. [stacktrace in console]`;
+
+			document.getElementById(`gpt_${window.token}`).innerHTML = error_message;
+			add_message(window.conversation_id, "assistant", error_message);
+		} else {
+			document.getElementById(`gpt_${window.token}`).innerHTML += ` [aborted]`;
+			add_message(window.conversation_id, "assistant", text + ` [aborted]`);
+		}
+
+		window.scrollTo(0, 0);
+	}
+};
+
+const clear_conversations = async () => {
+	const elements = box_conversations.childNodes;
+	let index = elements.length;
+
+	if (index > 0) {
+		while (index--) {
+			const element = elements[index];
+			if (
+				element.nodeType === Node.ELEMENT_NODE &&
+				element.tagName.toLowerCase() !== `button`
+			) {
+				box_conversations.removeChild(element);
+			}
+		}
+	}
+};
+
+const clear_conversation = async () => {
+	let messages = message_box.getElementsByTagName(`div`);
+
+	while (messages.length > 0) {
+		message_box.removeChild(messages[0]);
+	}
+};
+
+const show_option = async (conversation_id) => {
+	const conv = document.getElementById(`conv-${conversation_id}`);
+	const yes = document.getElementById(`yes-${conversation_id}`);
+	const not = document.getElementById(`not-${conversation_id}`);
+
+	conv.style.display = "none";
+	yes.style.display = "block";
+	not.style.display = "block";
+}
+
+const hide_option = async (conversation_id) => {
+	const conv = document.getElementById(`conv-${conversation_id}`);
+	const yes = document.getElementById(`yes-${conversation_id}`);
+	const not = document.getElementById(`not-${conversation_id}`);
+
+	conv.style.display = "block";
+	yes.style.display = "none";
+	not.style.display = "none";
+}
+
+const delete_conversation = async (conversation_id) => {
+	localStorage.removeItem(`conversation:${conversation_id}`);
+
+	const conversation = document.getElementById(`convo-${conversation_id}`);
+	conversation.remove();
+
+	if (window.conversation_id == conversation_id) {
+		await new_conversation();
+	}
+
+	await load_conversations(20, 0, true);
+};
+
+const set_conversation = async (conversation_id) => {
+	history.pushState({}, null, `/chat/${conversation_id}`);
+	window.conversation_id = conversation_id;
+
+	await clear_conversation();
+	await load_conversation(conversation_id);
+	await load_conversations(20, 0, true);
+};
+
+const new_conversation = async () => {
+	history.pushState({}, null, `/chat/`);
+	window.conversation_id = uuid();
+
+	await clear_conversation();
+	await load_conversations(20, 0, true);
+};
+
+const load_conversation = async (conversation_id) => {
+	let conversation = await JSON.parse(
+		localStorage.getItem(`conversation:${conversation_id}`)
+	);
+	console.log(conversation, conversation_id);
+
+	for (item of conversation.items) {
+		message_box.innerHTML += `
+            <div class="message">
+                <div class="user">
+                    ${item.role == "assistant" ? gpt_image : user_image}
+                    ${
+                      item.role == "assistant"
+                        ? `<i class="fa-regular fa-phone-arrow-down-left"></i>`
+                        : `<i class="fa-regular fa-phone-arrow-up-right"></i>`
+                    }
+                </div>
+                <div class="content">
+                    ${
+                      item.role == "assistant"
+                        ? markdown.render(item.content)
+                        : item.content
+                    }
+                </div>
+            </div>
+        `;
+	}
+
+	document.querySelectorAll('code:not(p code):not(li code)').forEach((el) => {
+		hljs.highlightElement(el);
+		el.classList.add('processed');
+	});
+
+	message_box.scrollTo({
+		top: message_box.scrollHeight,
+		behavior: "smooth"
+	});
+
+	setTimeout(() => {
+		message_box.scrollTop = message_box.scrollHeight;
+	}, 500);
+};
+
+const get_conversation = async (conversation_id) => {
+	let conversation = await JSON.parse(
+		localStorage.getItem(`conversation:${conversation_id}`)
+	);
+	return conversation.items;
+};
+
+const add_conversation = async (conversation_id, title) => {
+	if (localStorage.getItem(`conversation:${conversation_id}`) == null) {
+		localStorage.setItem(
+			`conversation:${conversation_id}`,
+			JSON.stringify({
+				id: conversation_id,
+				title: title,
+				items: [],
+			})
+		);
+	}
+};
+
+const add_message = async (conversation_id, role, content) => {
+	before_adding = JSON.parse(
+		localStorage.getItem(`conversation:${conversation_id}`)
+	);
+
+	before_adding.items.push({
+		role: role,
+		content: content,
+	});
+
+	localStorage.setItem(
+		`conversation:${conversation_id}`,
+		JSON.stringify(before_adding)
+	); // update conversation
+};
+
+const load_conversations = async (limit, offset, loader) => {
+	//console.log(loader);
+	//if (loader === undefined) box_conversations.appendChild(spinner);
+
+	let conversations = [];
+	for (let i = 0; i < localStorage.length; i++) {
+		if (localStorage.key(i).startsWith("conversation:")) {
+			let conversation = localStorage.getItem(localStorage.key(i));
+			conversations.push(JSON.parse(conversation));
+		}
+	}
+
+	//if (loader === undefined) spinner.parentNode.removeChild(spinner)
+	await clear_conversations();
+
+	for (conversation of conversations) {
+		box_conversations.innerHTML += `
+    <div class="convo" id="convo-${conversation.id}">
+      <div class="left" onclick="set_conversation('${conversation.id}')">
+          <i class="fa-regular fa-comments"></i>
+          <span class="convo-title">${conversation.title}</span>
+      </div>
+      <i onclick="show_option('${conversation.id}')" class="fa-regular fa-trash" id="conv-${conversation.id}"></i>
+      <i onclick="delete_conversation('${conversation.id}')" class="fa-regular fa-check" id="yes-${conversation.id}" style="display:none;"></i>
+      <i onclick="hide_option('${conversation.id}')" class="fa-regular fa-x" id="not-${conversation.id}" style="display:none;"></i>
+    </div>
+    `;
+	}
+
+	document.querySelectorAll('code:not(p code):not(li code)').forEach((el) => {
+		hljs.highlightElement(el);
+		el.classList.add('processed');
+	});
+};
+
+document.getElementById(`cancelButton`).addEventListener(`click`, async () => {
+	window.controller.abort();
+	console.log(`aborted ${window.conversation_id}`);
+});
+
+function h2a(str1) {
+	var hex = str1.toString();
+	var str = "";
+
+	for (var n = 0; n < hex.length; n += 2) {
+		str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+	}
+
+	return str;
+}
+
+const uuid = () => {
+	return `xxxxxxxx-xxxx-4xxx-yxxx-${Date.now().toString(16)}`.replace(
+		/[xy]/g,
+		function(c) {
+			var r = (Math.random() * 16) | 0,
+				v = c == "x" ? r : (r & 0x3) | 0x8;
+			return v.toString(16);
+		}
+	);
+};
+
+const message_id = () => {
+	random_bytes = (Math.floor(Math.random() * 1338377565) + 2956589730).toString(
+		2
+	);
+	unix = Math.floor(Date.now() / 1000).toString(2);
+
+	return BigInt(`0b${unix}${random_bytes}`).toString();
+};
+
+window.onload = async () => {
+	load_settings_localstorage();
+
+	conversations = 0;
+	for (let i = 0; i < localStorage.length; i++) {
+		if (localStorage.key(i).startsWith("conversation:")) {
+			conversations += 1;
+		}
+	}
+
+	if (conversations == 0) localStorage.clear();
+
+	await setTimeout(() => {
+		load_conversations(20, 0);
+	}, 1);
+
+	if (!window.location.href.endsWith(`#`)) {
+		if (/\/chat\/.+/.test(window.location.href)) {
+			await load_conversation(window.conversation_id);
+		}
+	}
+
+	message_input.addEventListener(`keydown`, async (evt) => {
+		if (prompt_lock) return;
+		if (evt.keyCode === 13 && !evt.shiftKey) {
+			evt.preventDefault();
+			console.log('pressed enter');
+			await handle_ask();
+		} else {
+			message_input.style.removeProperty("height");
+			message_input.style.height = message_input.scrollHeight + 4 + "px";
+		}
+	});
+
+	send_button.addEventListener(`click`, async () => {
+		console.log("clicked send");
+		if (prompt_lock) return;
+		await handle_ask();
+	});
+
+	register_settings_localstorage();
+};
+
+var toggleSwitch = document.getElementById("toggle-switch");
+toggleSwitch.addEventListener("change", function() {
+	if (toggleSwitch.checked) {
+		document.getElementById("light").checked = true;
+		var oldlink = document.getElementsByTagName("link").item(1);
+		var newlink = document.createElement("link");
+		newlink.setAttribute("rel", "stylesheet");
+		newlink.setAttribute("type", "text/css");
+		newlink.setAttribute("href", "assets/css/googlecode.min.css");
+		document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+	} else {
+		document.getElementById("dark").checked = true;
+		var oldlink = document.getElementsByTagName("link").item(1);
+		var newlink = document.createElement("link");
+		newlink.setAttribute("rel", "stylesheet");
+		newlink.setAttribute("type", "text/css");
+		newlink.setAttribute("href", "assets/css/dracula.min.css");
+		document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+	}
+});
+
+document.querySelector(".mobile-sidebar").addEventListener("click", (event) => {
+	const sidebar = document.querySelector(".conversations");
+
+	if (sidebar.classList.contains("shown")) {
+		sidebar.classList.remove("shown");
+		event.target.classList.remove("rotated");
+	} else {
+		sidebar.classList.add("shown");
+		event.target.classList.add("rotated");
+	}
+
+	window.scrollTo(0, 0);
+});
+
+const register_settings_localstorage = async () => {
+	settings_ids = ["model", "temperature"];
+	settings_elements = settings_ids.map((id) => document.getElementById(id));
+	settings_elements.map((element) =>
+		element.addEventListener(`change`, async (event) => {
+			switch (event.target.type) {
+				case "checkbox":
+					localStorage.setItem(event.target.id, event.target.checked);
+					break;
+				case "select-one":
+					localStorage.setItem(event.target.id, event.target.selectedIndex);
+					break;
+				default:
+					console.warn("Unresolved element type");
+			}
+		})
+	);
+};
+
+const load_settings_localstorage = async () => {
+	settings_ids = ["model", "temperature"];
+	settings_elements = settings_ids.map((id) => document.getElementById(id));
+	settings_elements.map((element) => {
+		if (localStorage.getItem(element.id)) {
+			switch (element.type) {
+				case "checkbox":
+					element.checked = localStorage.getItem(element.id) === "true";
+					break;
+				case "select-one":
+					element.selectedIndex = parseInt(localStorage.getItem(element.id));
+					break;
+				default:
+					console.warn("Unresolved element type");
+			}
+		}
+	});
+};
