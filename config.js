18312dabf3dd383f7771cafbf1fb25516c7e130c
Alex <132716380+alexsky177@users.noreply.github.com>
2023-06-04 02:23:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const strIndex = "Your API KEY"; // Fill OpenAI API Key Here
-const path = "chat";  // change path of chat in your server if you need
+const path = "chat";  // change path if you need
