81c25df2f00e6e976e491b4628f0049a4993c0f2
Alex <132716380+alexsky177@users.noreply.github.com>
2023-06-05 14:55:11 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
-const strIndex = "Your API KEY"; // Fill OpenAI API Key Here
+const API_URL = "https://api.openai.com/v1/chat/completions";
+const strIndex = ""; // Fill OpenAI API Key Here
 const path = "chat";  // change path if you need
