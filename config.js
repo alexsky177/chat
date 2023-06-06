0ff588af99f6d88ed8494a639c47802c4f03cfee
Alex <132716380+alexsky177@users.noreply.github.com>
2023-06-07 01:53:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const API_URL = "https://api.openai.com/v1/chat/completions";
-const strIndex = "Your_API_Here";
+const strIndex = "Your_API_Key";
 const path = "/chat";  // Change path to your path. For localhost make it path = "";
 // const root = "."; // This parameter need if you use app in the root or on localhost, delete 2 slashes at the begining of this string.
