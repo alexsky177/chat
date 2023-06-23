dac54f007e28423643718eddf494a3db100803d5
Alex <132716380+alexsky177@users.noreply.github.com>
2023-06-23 23:22:06 +0300

    Update config.js
    Added system message

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const API_URL = "https://api.openai.com/v1/chat/completions";
 const strIndex = "Your_API_Key";
-const system_message = "You are a smart robot!"; // You can change this massage
+const system_message = "You are a smart robot!"; // You can change this message
 const path = "/chat";  // Change path to your path. For localhost make it path = "";
 // const root = "."; // This parameter need if you use app in the root or on localhost, delete 2 slashes at the begining of this string.
